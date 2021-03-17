5a7bcaa1f6fdc91522a9062477df2b4646ece42d
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2021-03-17 20:11:35 +0100

    Add Pet model

--- src/pets/models.py
+++ src/pets/models.py
@@ -1,3 +1,17 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=255)
+    official_name = models.CharField(max_length=255, blank=True, default="")
+    date_of_birth = models.DateField(auto_now_add=True)
+    image = models.ImageField(blank=True)
+    species = models.CharField(max_length=255)
+    breed = models.CharField(max_length=255)
+    sex = models.CharField(max_length=255, blank=True, default="")
+    color = models.CharField(max_length=255, blank=True, default="")
+    chip_number = models.CharField(max_length=255, blank=True, default="")
+    passport_number = models.CharField(max_length=255, blank=True, default="")
